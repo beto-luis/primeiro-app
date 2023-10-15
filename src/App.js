856c491aa411fbbd2e3508766bbd08fbc9f57c3b
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-10-15 13:34:22 -0300

    prop children

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Card from './components/card/card'
 function App() {
   return (
     <>
-  <Card  title='Título card 1'/>
-  <Card  title='Título card 2'/>
-  <Card  title='Título card 3'/>
+      <Card>
+          <h3>Título card 1</h3>
+          <p>esse é um texto do card 1</p>
+      </Card>
+      <Card>
+          <h3>Título card 2</h3>
+          <p>esse é um texto do card 2</p>
+      </Card>
+      <Card>
+          <h3>Título card 3</h3>
+          <p>esse é um texto do card 3</p>
+      </Card>
   </>
   )
 }
